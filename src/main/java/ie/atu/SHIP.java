c6e5c4a543fe3bcb3a800896be4d2a2149ce2911
Tyrese <g00421432@atu.ie>
2023-12-11 10:21:33 +0000

    created 3 classes, Ship, ferry and trawler and made them extents of each other, added code

--- src/main/java/ie/atu/SHIP.java
+++ src/main/java/ie/atu/SHIP.java
@@ -1,5 +1,43 @@
 package ie.atu;
 
 public class SHIP {
+ private String name;
+ private String year;
+ private int crewNum;
+ private String vesselDetails;
+
+    public SHIP(String name, String year, int crewNum) {
+        this.name = name;
+        this.year = year;
+        this.crewNum = crewNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+
+        this.year = year;
+    }
+
+    public int getCrewNum() {
+        return crewNum;
+    }
+
+    public void setCrewNum(int crewNum) {
+        this.crewNum = crewNum;
+    }
+
+
+
 
 }
--- src/main/java/ie/atu/TRAWLER.java
+++ src/main/java/ie/atu/TRAWLER.java
@@ -1,4 +1,17 @@
 package ie.atu;
 
-public class TRAWLER {
+public class TRAWLER extends SHIP{
+    private int Capacity;
+
+    public TRAWLER(String name, String year, int crewNum, int capacity) {
+        super(name, year, crewNum);
+        Capacity = capacity;
+    }
+    public int getCapacity() {
+        return Capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        Capacity = capacity;
+    }
 }
