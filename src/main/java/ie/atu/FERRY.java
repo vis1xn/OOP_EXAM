4246123d7ae21188618ddaa2999e370a4d1de2b6
Tyrese <g00421432@atu.ie>
2023-12-11 10:27:44 +0000

    code was re-added to classes

--- src/main/java/ie/atu/FERRY.java
+++ src/main/java/ie/atu/FERRY.java
@@ -1,4 +1,17 @@
 package ie.atu;
 
-public class FERRY {
+public class FERRY extends SHIP{
+        private int passNum;
+
+        public FERRY(String name, String year, int crewNum, int passNum) {
+            super(name, year, crewNum);
+            this.passNum = passNum;
+        }
+        public int getPassNum() {
+            return passNum;
+        }
+
+        public void setPassNum(int passNum) {
+            this.passNum = passNum;
+        }
 }
